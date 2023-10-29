22f4b5e5f348b216fbd7d68b8586595c752cbd98
d0lim <limdongyoung0@gmail.com>
2023-10-29 17:58:30 +0900

    refactor: retrieveRouters to static function

--- src/main/kotlin/com/d0lim/domain/service/RouterSearch.kt
+++ src/main/kotlin/com/d0lim/domain/service/RouterSearch.kt
@@ -3,7 +3,9 @@ package com.d0lim.domain.service
 import com.d0lim.domain.entity.Router
 
 class RouterSearch {
-    fun retrieveRouters(routers: List<Router>, predicate: (Router) -> Boolean): List<Router> {
-        return routers.filter(predicate)
+    companion object {
+        fun retrieveRouters(routers: List<Router>, predicate: (Router) -> Boolean): List<Router> {
+            return routers.filter(predicate)
+        }
     }
 }
