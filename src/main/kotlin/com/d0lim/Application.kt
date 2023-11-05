e68d40db85589badaaa86255dfc71df4ffdb5812
d0lim <limdongyoung0@gmail.com>
2023-11-05 16:19:05 +0900

    feat: apply to main Application

--- src/main/kotlin/com/d0lim/Application.kt
+++ src/main/kotlin/com/d0lim/Application.kt
@@ -1,14 +1,18 @@
 package com.d0lim
 
-import com.d0lim.application.ports.input.RouterViewInputPort
-import com.d0lim.framework.adapters.input.stdin.RouterViewCLIAdapter
-import com.d0lim.framework.adapters.output.file.RouterViewFileAdapter
+import com.d0lim.application.ports.input.RouterNetworkInputPort
+import com.d0lim.domain.vo.IP
+import com.d0lim.domain.vo.Network
+import com.d0lim.domain.vo.RouterId
+import com.d0lim.framework.adapters.input.stdin.RouterNetworkCliAdapter
+import com.d0lim.framework.adapters.output.file.RouterNetworkFileAdapter
 
 fun main() {
-    val type = "EDGE"
+    val routerNetworkUseCase = RouterNetworkInputPort(RouterNetworkFileAdapter)
+    val cli = RouterNetworkCliAdapter(routerNetworkUseCase)
 
-    val routerViewUseCase = RouterViewInputPort(RouterViewFileAdapter)
-    val cli = RouterViewCLIAdapter(routerViewUseCase)
-
-    println(cli.obtainRelatedRouters(type))
+    val routerId = RouterId.withId("ca23800e-9b5a-11eb-a8b3-0242ac130003")
+    val network = Network(IP("20.0.0.0"), "Marketing", 8)
+    val router = cli.addNetwork(routerId, network)
+    println(router)
 }
