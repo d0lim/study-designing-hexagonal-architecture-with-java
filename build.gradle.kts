6f2058aeda7cb47cb7c106a2587aa367af01594c
d0lim <limdongyoung0@gmail.com>
2023-11-18 16:44:06 +0900

    feat: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    implementation("org.hibernate.orm:hibernate-core:6.3.1.Final")
+
+    runtimeOnly("com.h2database:h2:2.2.224")
+
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
